189c0914af4b957d437a1f93e10cee7a38f9f789
Marnix <marnix@valkdigital.nl>
2024-01-23 12:26:50 +0100

    Add more treesitters for different languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,23 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "lua", "vimdoc", "typescript" },
+				ensure_installed = {
+					"lua",
+					"vimdoc",
+					"typescript",
+					"tsx",
+					"html",
+					"css",
+					"javascript",
+					"json",
+					"yaml",
+          "elixir",
+          "eex",
+          "heex",
+          "surface",
+				},
+        sync_install = false,
+        auto_install = true,
 				highlight = { enable = true },
 			})
 		end,
