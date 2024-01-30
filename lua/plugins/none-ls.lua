6db68a86efcbbfc28c356ffe6651406ab5bf0386
Marnix <marnix@valkdigital.nl>
2024-01-30 11:01:51 +0100

    Add mason null ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,39 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "stylua",
+          "prettierd",
+          "trivy",
+          "yamllint",
+          "stylelint",
+        },
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.trivy,
-        null_ls.builtins.diagnostics.yamllint,
-        null_ls.builtins.diagnostics.stylelint,
-      },
-    })
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.diagnostics.trivy,
+          null_ls.builtins.diagnostics.yamllint,
+          null_ls.builtins.diagnostics.stylelint,
+        },
+      })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
