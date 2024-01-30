c9a61ffbe06a644b3d0364e3121a6d918854618c
Marnix <marnix@valkdigital.nl>
2024-01-30 10:53:38 +0100

    Update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,64 +1,96 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-					"elixirls",
-					"yamlls",
-					"jsonls",
-					"html",
-					"cssls",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "elixirls",
+          "yamlls",
+          "jsonls",
+          "html",
+          "cssls",
           "eslint",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.elixirls.setup({
-        cmd = { "/Users/marnixvalkdigital/elixir-ls/language_server.sh" },
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.elixirls.setup({
+        cmd = { "$HOME/elixir-ls/language_server.sh" },
+        capabilities = capabilities,
+      })
+      lspconfig.yamlls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
       lspconfig.eslint.setup({
         capabilities = capabilities,
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float(0, {scope=\"line\"})<CR>", {})
+      local function filter(arr, fn)
+        if type(arr) ~= "table" then
+          return arr
+        end
+
+        local filtered = {}
+        for k, v in pairs(arr) do
+          if fn(v, k, arr) then
+            table.insert(filtered, v)
+          end
+        end
+
+        return filtered
+      end
+
+      local function filterReactDTS(value)
+        return string.match(value.filename, "react/index.d.ts") == nil
+      end
+
+      local function on_list(options)
+        local items = options.items
+        if #items > 1 then
+          items = filter(items, filterReactDTS)
+        end
+
+        vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
+        vim.api.nvim_command("cfirst")
+      end
+
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition({ on_list = on_list })
+      end)
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>e", ':lua vim.diagnostic.open_float(0, {scope="line"})<CR>', {})
     end,
-	},
+  },
 }
