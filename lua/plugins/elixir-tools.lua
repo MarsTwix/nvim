33933f91fce147cc9ce71954f0e28c0d909efbaa
Marnix <marnix@valkdigital.nl>
2024-03-12 11:19:40 +0100

    Update elixir tools

--- lua/plugins/elixir-tools.lua
+++ lua/plugins/elixir-tools.lua
@@ -4,13 +4,18 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local elixir = require("elixir")
+    local elixirls = require("elixir.elixirls")
 
     elixir.setup({
       nextls = { enable = false },
       credo = { enable = true },
       elixirls = {
         enable = true,
-        tag = "v0.19.0",
+        tag = "v0.20.0",
+        settings = elixirls.settings({
+          dialyzerEnabled = false,
+          enableTestLenses = false,
+        }),
       },
     })
   end,
