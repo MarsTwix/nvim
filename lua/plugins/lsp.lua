6f9a25ff7ed32b5689e666a405b324c9a5f38cac
MarsTwix <marnixwildeman@gmail.com>
2025-05-06 14:52:31 +0200

    Add c# lsp and formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,7 +75,6 @@ return {
           vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
           vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
           vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-          vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
           vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
           vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.format()<cr>", opts)
           -- vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
@@ -85,7 +84,7 @@ return {
       })
 
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "html", "ts_ls" },
+        ensure_installed = { "lua_ls", "html", "ts_ls", "omnisharp" },
         handlers = {
           function(server_name)
             if server_name == "html" then
@@ -119,6 +118,7 @@ return {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.prettierd,
           null_ls.builtins.formatting.phpcsfixer,
+          null_ls.builtins.formatting.csharpier,
 
           -- Linters
           null_ls.builtins.diagnostics.phpcs,
