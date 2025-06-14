19237a03f02f4861d0b7e9fcaa3c3d2921e93e52
MarsTwix <marnixwildeman@gmail.com>
2025-06-14 16:20:10 +0200

    Add vim config with number

---
+++ lua/config/vim.lua
@@ -0,0 +1,8 @@
+-- Show numbers on the side
+vim.o.number = true
+-- Make the numbers relative of the current cursor position
+vim.o.relativenumber = true
+
+-- This is needed for file explorer plugin (nvim-tree/nvim-tree)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
