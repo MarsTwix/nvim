459b7cf2939ccef3dd14b201066658b24ce3c183
MarsTwix <marnixwildeman@gmail.com>
2025-06-14 16:20:18 +0200

    Add file explorer

---
+++ lua/plugins/file-explorer.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local tree = require("nvim-tree")
+
+    tree.setup({
+      view = {
+        side = "right",
+        adaptive_size = true,
+        number = true,
+        relativenumber = true,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = false,
+      },
+      filters = {
+        git_ignored = false,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+      update_focused_file = {
+        enable = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+  end,
+}
