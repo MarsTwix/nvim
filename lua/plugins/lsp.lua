cb2138b80c5ab43a4d21e0e63bd7ae1003008e9f
MarsTwix <marnixwildeman@gmail.com>
2025-06-16 00:48:24 +0200

    add lemminx for xml lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -84,7 +84,7 @@ return {
 			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jdtls" },
+				ensure_installed = { "lua_ls", "jdtls", "lemminx" },
 			})
 		end,
 	},
