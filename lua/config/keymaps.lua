55652e11b181b69e4e7ad9d32d775c7d553fc9f3
MarsTwix <marnixwildeman@gmail.com>
2025-06-16 00:15:23 +0200

    format all files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- Better window navigation
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Escape more easily
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,41 +1,41 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    local tree = require("nvim-tree")
+		local tree = require("nvim-tree")
 
-    tree.setup({
-      view = {
-        side = "right",
-        adaptive_size = true,
-        number = true,
-        relativenumber = true,
-      },
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        show_on_open_dirs = false,
-      },
-      filters = {
-        git_ignored = false,
-      },
-      actions = {
-        open_file = {
-          quit_on_open = true,
-        },
-      },
-      update_focused_file = {
-        enable = true,
-      },
-    })
+		tree.setup({
+			view = {
+				side = "right",
+				adaptive_size = true,
+				number = true,
+				relativenumber = true,
+			},
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+				show_on_open_dirs = false,
+			},
+			filters = {
+				git_ignored = false,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+			update_focused_file = {
+				enable = true,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-  end,
+		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+	end,
 }
