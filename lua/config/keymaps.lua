1726df423b86c4386ffc3f050020ee32e33b5d0b
MarsTwix <marnixwildeman@gmail.com>
2025-06-16 17:26:01 +0200

    Add buffer keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,9 @@ vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true 
 
 -- Escape more easily
 vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- Buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true })
+vim.keymap.set("n", "<S-Tab>", ":bprev<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", ":bdelete<CR>", { silent = true })
+vim.keymap.set("n", "<S-leader>X", ":bdelete!<CR>", { silent = true })
