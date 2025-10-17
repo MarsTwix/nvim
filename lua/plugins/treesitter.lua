0a0b1fbebeaf77b1a990ae650fd022868023607f
Marnix Wildeman <m.wildeman@agrifirm.com>
2025-10-17 15:20:21 +0200

    Update treesitter for noice

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,12 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	lazy = "false",
+	lazy = false,
 	branch = "main",
+	config = function()
+		ts = require("nvim-treesitter")
+
+		ts.setup({})
+		ts.install({ "vim" })
+	end,
 }
