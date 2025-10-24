18a9cd84291c64afb4fcd9e1b3a66d66c4ba8644
MarsTwix <55348159+MarsTwix@users.noreply.github.com>
2025-10-24 11:40:14 +0200

    Add transparent plugin

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,15 @@
+return {
+	"xiyaowong/transparent.nvim",
+	lazy = false,
+	config = function()
+		local transparent = require("transparent")
+		transparent.setup({
+			extra_groups = {
+				"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+				"NvimTreeNormal", -- NvimTree
+			},
+		})
+		transparent.clear_prefix("Telescope")
+		transparent.clear_prefix("lualine")
+	end,
+}
