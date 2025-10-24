f59824213595201feff20fe3bf7d48d09a3d1ec3
MarsTwix <55348159+MarsTwix@users.noreply.github.com>
2025-10-24 11:40:25 +0200

    Fix noice warning

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,7 +10,14 @@ return {
 	end,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
+		{
+			"rcarriga/nvim-notify",
+			config = function()
+				require("notify").setup({
+					background_colour = "#000000",
+				})
+			end,
+		},
 		"nvim-treesitter/nvim-treesitter",
 	},
 }
