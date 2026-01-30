c4c3c378300dfced7a94e6416a39d8800e110146
MarsTwix <55348159+MarsTwix@users.noreply.github.com>
2026-01-30 17:57:18 +0100

    Add codex cli chat

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,45 @@
+local codex_term
+
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		size = function(term)
+			if term.direction == "vertical" then
+				return 70
+			end
+			return 15
+		end,
+		persist_size = false,
+		open_mapping = [[<c-\>]],
+		shade_terminals = true,
+		direction = "float",
+		float_opts = {
+			border = "rounded",
+		},
+		on_open = function(term)
+			if term.direction == "vertical" then
+				vim.cmd("wincmd H")
+				vim.api.nvim_win_set_width(0, 70)
+				vim.wo.winfixwidth = true
+			end
+		end,
+	},
+	keys = {
+		{
+			"<leader>tc",
+			function()
+				local Terminal = require("toggleterm.terminal").Terminal
+				if not codex_term then
+					codex_term = Terminal:new({
+						cmd = "codex",
+						direction = "vertical",
+						hidden = true,
+					})
+				end
+				codex_term:toggle()
+			end,
+			desc = "Codex terminal (left)",
+		},
+	},
+}
